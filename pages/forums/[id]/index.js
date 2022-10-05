f4fedd1e655cb53033cdabd15612fc0851563927
Bryan Lilly <bryanjameslilly@gmail.com>
2022-10-04 22:14:14 -0600

    added threads list to forum

--- pages/forums/[id]/index.js
+++ pages/forums/[id]/index.js
@@ -1,41 +1,47 @@
-// import React from 'react'
-// import Thread from '../../../src/Thread/Thread'
-
-// const Forum = () => {
-//   return (
-//     <>
-//       <div className='m-5'>
-//         <h1 className='mb-3 text-3xl font-bold'>Trade Rumors and Free Agent Talk</h1>
-//         <p>Trade rumors, transactions, and free agent talk. Rumors must use the RUMOR prefix in thread title. Proposals must contain the PROPOSAL prefix in the thread title.</p>
-//       </div>
-//       <Thread />
-//       <Thread />
-//     </>
-//   )
-// }
-
-// export default Forum
-
-import { loadGetInitialProps } from "next/dist/shared/lib/utils";
-import { useRouter } from "next/router";
-
 import React from 'react'
 
-const Forum = ({data}) => {
-  const router = useRouter();
-  const {id} = router.query
+const Forum = (props) => {
   return (
-    <div>
-      <h1>{data.attributes.title}</h1>
-    </div>
+    <>
+      <div className="flex">
+        <h1 className="m-auto mt-5 mb-5 text-2xl">{props.data.data.attributes.title}</h1>
+      </div>
+
+      {/* THREAD WRAPPER */}
+      <div className="w-full h-full px-5">
+        {
+          props.data.data.attributes.threads.data.map(thread => 
+            // THIS SHOULD BE A COMPONENT
+            <>
+              <div className="w-full h-36 border border-black mt-2 rounded-sm grid grid-cols-4 cursor-pointer shadow-lg">
+                <div className='col-span-1 border-r-2'></div>
+                <div className='col-span-3 grid grid-rows-2'>
+                  <div className='p-1'>
+                    <h1 className='w-full'>{thread.attributes.title}</h1>
+                  </div>
+                  <div className='border border-t-2'>
+                    {/* NAME OF CREATOR */}
+
+                    {/* NUMBER OF RESPONSES */}
+                  </div>
+                </div>             
+              </div>
+            </>
+          )
+        }
+      </div>
+    </>
   )
 }
 
-Forum.getInitialProps = async ({query}) => {
-  const res = await fetch(`http://localhost:1337/api/forums/${query.id}`)
+export async function getServerSideProps(context){
+  const res = await fetch(`http://localhost:1337/api/forums/${context.params.id}?populate=*`)
   const data = await res.json();
-  //console.log(data)
-  return data;
+  console.log(data.data.attributes.threads.data[0].title)
+  // CAN REDUCE SIZE OF OBJECT
+  return {props:{
+    data
+  }};
 }
 
 export default Forum
