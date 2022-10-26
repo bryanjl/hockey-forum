d1d46e225df0cc93d0419dcf05ea7e42e3f353f9
Bryan Lilly <bryanjameslilly@gmail.com>
2022-10-25 20:58:05 -0600

    made active links for sponsors

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Layout = ({ children }) => {
   return (
@@ -25,11 +26,16 @@ const Layout = ({ children }) => {
         <div className='lg:col-span-1'>
           <div className='lg:border-l-2 lg:p-10 h-full'>
             {/* links to sponsors/etc */}
-            <div className='lg:w-full lg:mt-5 lg:block hidden'>
-              <Image src='/4th_line_voice.png' width={500} height={500} alt='4th Line Voice' />
+            
+            <div className='lg:w-full lg:mt-5 lg:block hidden  cursor-pointer'>
+              <Link href='https://link.chtbl.com/4thlinevoice'>
+                <Image src='/4th_line_voice.png' width={500} height={500} alt='4th Line Voice' />
+              </Link>
             </div>
-            <div className='lg:w-full lg:mt-5 lg:block hidden'>
-              <Image src='/five_for_fighting.png' width={500} height={500} alt='Five For Fighting' />
+            <div className='lg:w-full lg:mt-5 lg:block hidden cursor-pointer'>
+              <Link href='https://www.sixpackcoverage.com/five-for-fighting/'>
+                <Image src='/five_for_fighting.png' width={500} height={500} alt='Five For Fighting' />
+              </Link>
             </div>
           </div>
         </div>
